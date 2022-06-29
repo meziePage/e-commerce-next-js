9dd813b20289a6bea3c0805e1c1bba47ea1cc3e1
Ikemma3082 <80706383+Ikemma3082@users.noreply.github.com>
2022-06-29 08:51:28 -0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+import data from "../utils/data"
+import ProductItem from "../components/ProductItem"
+
+export default function Home() {
+  return <Layout title="Home Page">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {data.products.map((product) => (
+        <ProductItem product={product} key={product.slug} />
+      ))}
+    </div>
+  </Layout>;
+}
